edd42b0aaa57847db65820b4874d10faa49197d5
cuongth95 <icechanz@gmail.com>
2019-03-19 02:03:45 +0100

    _Add beacons & lines as demo map

---
+++ Beacon.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPen, QBrush
+
+'''
+@author Truong Huy Cuong
+'''
+
+class Beacon:
+    idCounter = 0
+    @staticmethod
+    def AutoGenId():
+        index = Beacon.idCounter+ 1
+        Beacon.idCounter +=1
+        return index
+
+    def __init__(self,startPos,doGenId=True):
+        if doGenId:
+            self.id = Beacon.AutoGenId()
+        else:
+            self.id = -1
+
+        self.size = 10
+        self.rsize = np.array([self.size,self.size])
+        self.pos = np.copy(startPos)
+
+    def Draw(self,qp):
+        origin = np.array([self.pos[0] - self.rsize[0], self.pos[1] - self.rsize[0]])
+        brush = QBrush(Qt.darkBlue)
+        qp.setBrush(brush)
+        qp.drawEllipse(origin[0], origin[1], self.rsize[0] * 2, self.rsize[0] * 2)
